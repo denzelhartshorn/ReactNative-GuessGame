d84fff614095fbccace35205f67f701ce6b5328e
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-12-07 16:05:42 -0800

    created and started Card.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,14 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
-    alignItems: "center"
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5,
+    backgroundColor: "white",
+    padding: 20
   },
   buttonContainer: {
     flexDirection: "row",
