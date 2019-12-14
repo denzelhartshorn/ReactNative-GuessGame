a3c57ea5c58df01a595c223cfb08a2e3a6f59a68
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-12-13 16:17:13 -0800

    added local  images

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import TitleText from "./TitleText";
 import Colors from "../constants/colors";
 
 const Header = props => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <TitleText>{props.title}</TitleText>
     </View>
   );
 };
@@ -15,16 +16,10 @@ const styles = StyleSheet.create({
   header: {
     width: "100%",
     height: 90,
-    //account for status bar or notch on phone
     paddingTop: 36,
     backgroundColor: Colors.primary,
     alignItems: "center",
     justifyContent: "center"
-  },
-  headerTitle: {
-    color: "black",
-    fontSize: 18,
-    fontFamily: "open-sans-bold"
   }
 });
 
